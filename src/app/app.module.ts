daa7ce6971b0fe08a961ec982c3bd746f1c66eff
itijain12 <itijain12@github.com>
2018-05-24 16:07:35 +0530

    Adding routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,45 +6,45 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
-
+import { GalleryComponent } from './gallery/gallery.component';
 export const routes: Routes = [
-  {
-      path: 'home',
-      component: HomeComponent
-  },
-  {
-      path: 'about',
-      component: AboutComponent
-  },
-  {
-      path: 'courses',
-      component: ContactComponent
-  },
-  {
-      path: '',
-      redirectTo: '/home',
-      pathMatch: 'full'
-  },
-  {
-      path: '**',
-      redirectTo: '/home',
-      pathMatch: 'full'
-  }
+    {
+        path: 'home',
+        component: HomeComponent
+    },
+    {
+        path: 'about',
+        component: AboutComponent
+    },
+    {
+        path: 'contact',
+        component: ContactComponent
+    },
+    {
+        path: 'gallery',
+        component: GalleryComponent
+    },
+    {
+        path: '',
+        redirectTo: '/home',
+        pathMatch: 'full'
+    }
 ];
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent,
-    AboutComponent,
-    ContactComponent
-  ],
-  imports: [
-    BrowserModule,
-    HttpClientModule,
-    RouterModule.forRoot(routes)
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        HomeComponent,
+        AboutComponent,
+        ContactComponent,
+        GalleryComponent
+    ],
+    imports: [
+        BrowserModule,
+        HttpClientModule,
+        RouterModule.forRoot(routes)
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
